064479b36a20d24972e57ad6f9710d8076b72856
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2021-12-15 21:41:16 -0300

    criando arquivos

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
-const http = require('http');
-const fs = require('fs'); //Permite manipular arquivos
+const http = require('http');  //Permite Manipular Servidores Locais
+const fs = require('fs'); //Permite Manipular Arquivos
 
 const hostname = "127.0.0.1";
-const port = 3000; // Porta do servidor
+const port = 3000; // Porta do Servidor
  
 const server = http.createServer((req,res)=>{ //Cria servidor local
 
-    if(req.url == '/andrade'){ // Verifica se 
+    if(req.url == '/andrade'){ // Verifica se a url de acesso é "/andrade" (http://localhost:3000/andrade)
 
-        fs.readFile('index.html',function(err,data){
-
+        fs.readFile('index.html',function(err,data){ //Mostra o arquivo index.html quando a página for aberta
+                                                                        // através do comando "node index.js" no cmd
             res.writeHead(200,{'Content-Type':'text/html'});
             res.write(data);
             return res.end()
             
         })
+
     }else{
 
-        return res.end();
+        return res.end(); //Sem esse código, o servidor fica carregando para sempre, caso a url não for (http://localhost:3000/andrade)
     }
     
 })
 
-server.listen(port,hostname,()=>{
+server.listen(port,hostname,()=>{  // Mostra a mensagem do "console.log" no cmd quando o servidor é inciado
 
-    console.log('vamobora');
+    console.log('O servidor está funcionando!');
 })
 
+//----------- CRIAR ARQUIVOS ------------//
+
+// Primeiro modo:
+// Se o arquivo já existir, ele é reescrito!
+
+//parâmetros: fs.writeFile('nome do arquivo e extensão', 'conteudo do arquivo', 'callback, caso dê certo' );
+
+fs.writeFile('nome.txt','teste arquivo estudo nodeJS', function(err){ //Neste caso, o callback é uma função
+
+    if(err) throw err;
+    console.log('O arquivo foi um sucesso');
+})
 
 
 
